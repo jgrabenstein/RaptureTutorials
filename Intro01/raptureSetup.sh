5af6d9df7898f420800041dc289b9fc6d2911fce
Zannie <Zannie.Alvarez@incapturetechnologies.com>
2016-02-04 10:15:46 -0800

    RCM-236 Infrastructure to download ReflexRunner. Need to figure out way around 302 status from download request.

--- Intro01/raptureSetup.sh
+++ Intro01/raptureSetup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # TODO: update HOST
-HOST="http://192.168.99.100:8080"
+HOST="http://192.168.99.100:8080" #http://etienne.incapture.net:8080
+REFLEX_RUNNER_FILE_NAME="ReflexRunner-2.0.0.20160202182104.zip"
+REFLEX_RUNNER_URL="https://github.com/RapturePlatform/Rapture/releases/download/3.0.0.20160203122306/$REFLEX_RUNNER_FILE_NAME"
+DEFAULT_REFLEX_RUNNER_PATH="/opt/rapture"
 
+if false; then
 read -p "Enter Etienne User: " user
 read -s -p "Enter Etienne Password: " pass
 echo $'\n'
+fi
+user="testuser2"
+pass="testuser2"
 
 hashpass=$(echo $pass | md5)
 
@@ -22,6 +29,69 @@ function validate_curl_response {
     exit 1
   fi
 }
+
+prompt_to_download=true
+while $prompt_to_download; do
+  read -p "Would you like to download ReflexRunner? [Y/n] " response
+  if [ -z "$response" ]; then
+    response="Y"
+  fi
+
+  case "$response" in
+    Y*|y*) get_download_path=true
+           prompt_to_download=false
+      ;;
+    N*|n*) get_download_path=false
+           prompt_to_download=false
+      ;;
+    *)
+           prompt_to_download=true
+      ;;
+  esac
+done
+
+do_download=false
+while $get_download_path; do
+  read -p "Enter the directory where you would like to save it, or 'skip' to cancel. [$DEFAULT_REFLEX_RUNNER_PATH] " directory
+  if [ -z "$directory" ]; then
+    directory=$DEFAULT_REFLEX_RUNNER_PATH
+    get_download_path=false
+    do_download=true
+  elif [ "$directory" = "skip" ]; then
+    get_download_path=false
+    do_download=false
+  elif [ "$directory" != "$DEFAULT_REFLEX_RUNNER_PATH" ] && [ ! -d "$directory" ]; then
+    echo "Path $directory doesn't exist."
+  elif [ ! -w "$directory" ]; then
+    echo "Path $directory exists but cannot be written to."
+  else
+    get_download_path=false
+    do_download=true
+  fi
+done
+
+if $do_download; then
+  # we will create the default directory for the user if we can, just not other directories for now
+  if mkdir -p $directory ; then
+    directory=${directory%/} # remove trailing slash
+    echo $(date)
+    echo "Downloading ReflexRunner to $directory. Please wait."
+
+    #curl_results=$( curl -qSsw '\n%{http_code}' --cookie .cookiefile $REFLEX_RUNNER_URL )
+    curl_results=$( curl -qSsw '\n%{http_code}' $REFLEX_RUNNER_URL )
+    validate_curl_response $? $(echo "$curl_results" | tail -n1) "There was a problem downloading ReflexRunner from $REFLEX_RUNNER_URL."
+    zip_file_data=$(echo "$curl_results" | sed \$d) #strip http status
+    echo $zip_file_data >> "$directory/$REFLEX_RUNNER_FILE_NAME"
+
+    echo "Done downloading."
+  else
+    echo "Unable to create directory $directory."
+    exit 1
+  fi
+fi
+
+exit 0
+
 
 curl_results=$( curl -qSsw '\n%{http_code}' --cookie-jar .cookiefile $login_url )
 validate_curl_response $? $(echo "$curl_results" | tail -n1) "There was a problem logging into $HOST."
