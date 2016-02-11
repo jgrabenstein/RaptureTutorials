8655e3ea33bb858b7e8041b7c83e5081a66c992a
Zannie <Zannie.Alvarez@incapturetechnologies.com>
2016-02-10 19:21:12 -0800

    UNFILED Frigging host again.

--- Setup/rapture.sh
+++ Setup/rapture.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HOST="http://192.168.99.100:8080"
+HOST="http://etn.incapture.net:8080"
 REFLEX_RUNNER_LATEST_HOST="https://github.com"
 REFLEX_RUNNER_LATEST="$REFLEX_RUNNER_LATEST_HOST/RapturePlatform/Rapture/releases/latest"
 
